a9e4c504c9204348ce3aba2b25bc3e34dd6911a7
jp <jp@jp10k.local>
2008-03-06 19:45:34 +0100

    Added throbber image files + Errors.js

---
+++ http/js/OpenPanelBeta/Errors.js
@@ -0,0 +1,30 @@
+var RPCError = function(message, status){
+	var err = new Error(message)
+    // take care of IE5/5.5
+    if (!err.message) {
+        err.message = message
+    }
+	
+	if(status != ""){
+		err.status = status;
+	} else {
+		err.status = "";
+	}
+	
+    err.name = "RPCError"
+    return err
+}
+
+var OpenCoreError = function(message, errorCode){
+	var err = new Error(message)
+    // take care of IE5/5.5
+    if (!err.message) {
+        err.message = message
+	}
+	
+	err.errorCode = errorCode!=undefined?errorCode:"";
+    err.name = "OpenCoreError"
+    return err
+}
+
+	
