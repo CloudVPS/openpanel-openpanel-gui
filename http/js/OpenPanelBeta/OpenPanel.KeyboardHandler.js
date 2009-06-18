dc96306ef82686ba53b020bc0a061ba22a8df953
Pim van Riezen <pi@openpanel.com>
2009-06-18 11:27:07 +0200

    typo

--- http/js/OpenPanelBeta/OpenPanel.KeyboardHandler.js
+++ http/js/OpenPanelBeta/OpenPanel.KeyboardHandler.js
@@ -16,7 +16,7 @@ OpenPanel.KeyboardHandler = {
 		this.Shortcuts.add ("esc",function(){hook.cancelfunc();});
 		this.Shortcuts.add ("enter",function(){console.log('enter'); hook.okfunc();},{'disable_in_input':false});
 		this.Shortcuts.add ("Meta+n",function(){
-			var bt document.getElementById("itemListAddButton");
+			var bt = document.getElementById("itemListAddButton");
 			if (bt != undefined) bt.onclick();
 			});
 		this.initialized = true;
