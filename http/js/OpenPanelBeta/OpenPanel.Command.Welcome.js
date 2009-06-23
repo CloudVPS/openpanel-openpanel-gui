f03c9ca76d775da0650bdc98b3b2370abdaaa9d9
Pim van Riezen <pi@openpanel.com>
2009-06-23 22:52:22 +0200

    logo size on welcome page

--- http/js/OpenPanelBeta/OpenPanel.Command.Welcome.js
+++ http/js/OpenPanelBeta/OpenPanel.Command.Welcome.js
@@ -14,8 +14,41 @@ OpenPanel.Command.Welcome = {
 		fullWindowAreaDiv.appendChild(welcomeDivHolder);
 		fullWindowAreaDiv.style.visibility = "visible";
 		this.controller.guiBuilder.loadTemplate("dynamic/", "welcomeDivHolder");
+		
+		var holder = $j("#welcomeLogoHolder");
+		if (holder != undefined) {
+			var logo = $("#welcomeLogo");
+			var h = this.pageHeight();
+			var curWidth = logo.width;
+			var curHeight = logo.height;
+			var height = curHeight;
+			var width = curWidth;
+			
+			if ((curHeight+450) > h) {
+				if (h>480) {
+					height = h-450;
+				} else height = 30;
+				
+				width = curWidth * (height / curHeight);
+				holder.style.width = width;
+				holder.style.height = height;
+				holder.style.marginLeft = width/2;
+				logo.width = width;
+				logo.height = height;
+			}
+		}
+		
 		this.controller.guiBuilder.GUIElements.IconBar.highliteItem("Welcome");
 		this.controller.guiBuilder.GUIElements.IconBar.setTitle("Welcome");
 		this.controller.guiBuilder.GUIElements.FormBuilder.setSaveButtonVisibility(false);
 	}
+
+	pageHeight: function() {
+		if (window.innerHeight != null) return window.innerHeight;
+		if (document.documentElement && document.documentElement.clientHeight) {
+			return document.documentElement.clientHeight;
+		}
+		if (document.body != null) return document.body.clientHeight;
+		return 500;
+	};
 }
