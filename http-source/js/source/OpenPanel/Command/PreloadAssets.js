bebf5d2b0ef23eddcc131b979d401790a31b0a1e
jp10k <jp@jp10000.com>
2010-10-11 21:58:35 +0200

    new preloader

---
+++ http-source/js/source/OpenPanel/Command/PreloadAssets.js
@@ -0,0 +1,30 @@
+OpenPanel.Command.PreloadAssets  = {
+	controller : {},
+	execute : function(actionObject){
+		
+		
+		
+		var url = "/dynamic/imagelist.json";
+		var that = this;
+		new Ajax.Request(url, {
+			  method:"get",
+			  onSuccess : that.loadDone.bind(that)
+		});
+	},
+	
+	loadDone : function(transport){
+		var assetList = transport.responseText.evalJSON();
+		assetList.each(function(src){
+			var i = new Image(32,32);
+			i.src = src;
+		});
+		
+		this.finish();
+	},
+	
+	finish : function(){
+		if(this.nextAction){
+			this.controller.action(this.nextAction);
+		}
+	}
+}
