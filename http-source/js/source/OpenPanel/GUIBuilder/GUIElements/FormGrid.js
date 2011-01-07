67c4b3f841670c0f442c65d7e9003fcb155e3458
Pim van Riezen <pi@openpanel.com>
2011-01-07 20:49:07 +0100

    Use a temporary lame way to determine the width of formgrids. Obviously we need some form of live resizing going forward.

--- http-source/js/source/OpenPanel/GUIBuilder/GUIElements/FormGrid.js
+++ http-source/js/source/OpenPanel/GUIBuilder/GUIElements/FormGrid.js
@@ -62,6 +62,20 @@ OpenPanel.GUIBuilder.GUIElements.FormGrid.prototype = {
 		var parameters = this.openCoreObject.getClassInfo().structure.parameters;
 		var createObject = {};
 		var classInfo = this.openCoreObject.getClassInfo();
+		var wantedWidth = 0;
+		
+		if (document.documentElement && document.documentElement.clientHeight)
+		{
+			wantedWidth = document.documentElement.clientHeight - 320;
+		}
+		else if (document.body)
+		{
+			wantedWidth = document.body.clientWidth - 320;
+		}
+		else
+		{
+			wantedWidth = self.innerWidth - 320;
+		}
 
 		for(var key in parameters){
 			var parameter = parameters[key];
@@ -86,13 +100,15 @@ OpenPanel.GUIBuilder.GUIElements.FormGrid.prototype = {
 		}
 		this.grid = new OpenPanel.GUIBuilder.GUIElements.Grid();
 		
+		wantedWid
+		
 		if (this.openCoreObject.meta == false && this.openCoreObject.canDelete == false && this.openCoreObject.canCreate == false)
 		{
-			this.grid.createInline(this.gridDiv,createObject,600, classInfo["class"].gridheight);
+			this.grid.createInline(this.gridDiv,createObject,wantedWidth, classInfo["class"].gridheight);
 		}
 		else
 		{
-			this.grid.createInlineWithButtons(this.gridDiv, createObject, 600, classInfo["class"].gridheight);
+			this.grid.createInlineWithButtons(this.gridDiv, createObject, wantedWidth, classInfo["class"].gridheight);
 			if (this.openCoreObject.meta == false) {
 				if (this.openCoreObject.canDelete == false) {
 					this.grid.disableDeleteButton();
